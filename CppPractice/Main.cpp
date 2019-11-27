dcbe973af2259c0befd25f4c4c8bf3afe463047d
PtahS <ptahsrg@gmail.com>
2019-11-27 21:12:36 +0200

    Sum of all even numbers in a range

--- CppPractice/Main.cpp
+++ CppPractice/Main.cpp
@@ -1,54 +1,25 @@
 #include <iostream>
 using namespace std;
-//ghghg
+
+// Count sum of all even numbers in a range.
+
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	while (true)
+	cout << "Enter lower range:   ";
+	int lowerRange;
+	cin >> lowerRange;
+
+	cout << "Enter bigger range:   ";
+	int biggerRange;
+	cin >> biggerRange;
+
+	int sum = 0;
+	for (lowerRange; lowerRange < biggerRange; lowerRange++)
 	{
-		cout << "Enter count of symbols:   ";
-		int count;
-		cin >> count;
-		cout << "Enter symbol:   ";
-		char symbol;
-		cin >> symbol;
-		cout << "Enter direction of row:" << endl
-			<< "1 - horizontal," << endl
-			<< "2 - vertical" << endl
-			<< "Enter suitable number:   ";
-
-		bool check = false;
-		char lineType = ' ';
-		while (check == false)
-		{
-
-			cin >> lineType;
-			if (lineType == '1' || lineType == '2')
-			{
-				check = true;
-			}
-			else
-			{
-				cout << "Enter number '1' or '2':   ";
-			}
-		}
-		cout << endl;
-		int index = 0;
-		while (index < count)
-		{
-			if (lineType == '1')
-			{
-				cout << symbol << ' ';
-			}
-			else
-			{
-				cout << symbol << endl;
-			}
-			index++;
-		}
-		cout << endl;
-		system("pause");
-		system("cls");
+		if (lowerRange % 2 == 0)
+			sum += lowerRange;
 	}
+	cout << "Sum of all even numbers is   " << sum << endl;
+	
 	return 0;
 }
