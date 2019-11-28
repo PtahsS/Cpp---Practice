85c6ce0afea750e7a83f2dbbb1ce7506000a6523
PtahS <ptahsrg@gmail.com>
2019-11-28 10:55:08 +0200

    Change values of variables in function

--- CppPractice/Main.cpp
+++ CppPractice/Main.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// Functions, link, pointer
-void Foo1(int a)
+// Change values of variables in function 
+
+void Foo(int& a, int& b, int& c)
 {
 	a = 2;
-}
-
-void Foo2(int* a)			//Pointer
-{
-	a = 100;		
-}
-
-void Foo3(int& a)			//Link
-{
-	a = 200;
+	b = 3;
+	c = 4;
 }
 
 int main()
 {
 	int a = 10;
-	cout << "a is " << a << endl;
-	Foo1(a);
-	cout << "After Foo1(a)  " << a << endl;
-	Foo2(&a);
-	cout << "After Foo2(a)  " << a << endl;
-	Foo3(a);
-	cout << "After Foo3(a)  " << a << endl;
+	int b = 20;
+	int c = 30;
+	Foo(a, b, c);
+	cout << a << "  " << b << "  " << c << endl;
 	return 0;
 }
