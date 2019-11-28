0483944c6ba92bb39e9dc46b6f3e89b4ef7811b2
PtahS <ptahsrg@gmail.com>
2019-11-28 22:35:11 +0200

    Conditional compilation. ifdef else endif

--- CppPractice/Main.cpp
+++ CppPractice/Main.cpp
@@ -1,21 +1,16 @@
 #include<iostream>
+#define DEBUG 8
 using namespace std;
-#define DEBUG
 
-//Conditional compilation. DEBUG
+//Conditional compilation. ifdef else endif
 
 int main()
 {
-	int value = 0;
-#ifdef DEBUG
-	cout << "Begin to start" << endl;
+#if DEBUG == 10
+	cout << "I love mathematic";
+#elif DEBUG>5
+	cout << "I love english";
 #endif
-	for (int i = 0; i < 10; i++)
-	{
-		value++;
-	}
-#ifdef DEBUG
-	cout << "End of circle" << endl;
-#endif
+	
 	return 0;
 }
