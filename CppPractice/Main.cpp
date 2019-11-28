0ae98aedc3a2e600ce53f11e592d64f604bb81d4
PtahS <ptahsrg@gmail.com>
2019-11-29 00:02:26 +0200

    Class Human

--- CppPractice/Main.cpp
+++ CppPractice/Main.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
-// Conditional ternary operator
+// Class Human. OOP
 
+class Human
+{
+public:
+	int age;
+	string name;
+	int weight;
+
+private:
+};
 int main()
 {
-	int a;
-	cout << "Enter your number:  ";
-	cin >> a;
-	a < 10 ? cout << "It is smaller then 10" :
-		a>10 ? cout << "It is bigger then 10" :
-		cout << "It is 10!!!";
-	
+	Human Petrov;
+	Petrov.age = 30;
+	Petrov.name = "Ivan";
+	Petrov.weight = 100;
+
+	cout << "My name is " << Petrov.name << ", I am "
+		<< Petrov.age << " years old. My weight is "
+		<< Petrov.weight << " kg.";
+
 	return 0;
 }
