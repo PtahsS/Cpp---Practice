82741bcf7ef07530e030241654bf08c29b6ea6e1
PtahS <ptahsrg@gmail.com>
2019-11-28 10:49:38 +0200

    Functions, link, pointer

--- CppPractice/Main.cpp
+++ CppPractice/Main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 using namespace std;
 
-// Link and pointer
+// Functions, link, pointer
+void Foo1(int a)
+{
+	a = 2;
+}
+
+void Foo2(int* a)			//Pointer
+{
+	a = 100;		
+}
+
+void Foo3(int& a)			//Link
+{
+	a = 200;
+}
 
 int main()
 {
-	int a = 5;
-	int* pa = &a;
-	int& aref = a;
-
-
-	cout << "a: " << a << endl;
-	cout << "aref" << '\t' << aref << endl;
-	aref = aref + 2;
-	cout << "aref" << '\t' << aref << endl;
-	aref = 555;
-	cout << "a: " << a << endl;
-	cout << "pa:  " << pa << endl
-		<< "*pa:  " << *pa << endl;
+	int a = 10;
+	cout << "a is " << a << endl;
+	Foo1(a);
+	cout << "After Foo1(a)  " << a << endl;
+	Foo2(&a);
+	cout << "After Foo2(a)  " << a << endl;
+	Foo3(a);
+	cout << "After Foo3(a)  " << a << endl;
 	return 0;
 }
