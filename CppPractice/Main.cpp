8d955c54703b42cfbbe3237b4dd656b9c35b8098
PtahS <ptahsrg@gmail.com>
2019-11-29 09:21:19 +0200

    Overloading parameter classes

--- CppPractice/Main.cpp
+++ CppPractice/Main.cpp
@@ -1,25 +1,60 @@
 #include <iostream>
 using namespace std;
 
-//Encapsulation example "CompOn"
+// Overloading parameter classes
 
-class CompOn
+class Point
 {
 public:
-	void Start()
+	Point()
 	{
-		isWorking() ? cout << "It is working" : cout << "You have to repair it.";
+		this->x = 0;
+		this->y = 0;
+	}
+	Point(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+	void SetX(int x)
+	{
+		this->x = x;
+	}
+	void SetY(int y)
+	{
+		this->y = y;
+	}
+	int GetX()
+	{
+		return this->x;
+	}
+	int GetY()
+	{
+		return this->y;
+	}
+	void Print()
+	{
+		cout << x << " : " << y << endl;
 	}
 private:
-	bool isWorking()
-	{
-		return false;
-	}
+	int x;
+	int y;
 };
+
 int main()
 {
-	CompOn a;
-	a.Start();
+	int x = 4;
+	int y = 5;
+	Point a;
+	a.Print();
+	a.SetX(10);
+	a.Print();
+	Point b(6, 17);
+	b.Print();
+	int c = b.GetY();
+	cout << c;
+
 
 	return 0;
 }
+
