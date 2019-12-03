01d867623a5ce71c1a14e8b2f1da43b011a0bc45
PtahS <ptahsrg@gmail.com>
2019-12-03 18:52:49 +0200

    Overloading operators (increment, decrement...)

--- CppPractice/Main.cpp
+++ CppPractice/Main.cpp
@@ -1,141 +1,113 @@
 #include <iostream>
 using namespace std;
 
-//class String (handmade)
+// Overloading operators (increment, decrement...)
 
-class MyString
+class Point
 {
 public:
-	MyString()
+	Point()
 	{
-		this->size = 0;
-		this->arr = nullptr;
+		x = 0;
+		y = 0;
 	}
-	MyString(const char* arr)
+	Point(int valueX, int valueY)
 	{
-		this->size = strlen(arr);
-		this->arr = new char[this->size+1];
-		for (int i = 0; i < this->size; i++)
-		{
-			this->arr[i] = arr[i];
-		}
-		this->arr[this->size] = '\0';
+		x = valueX;
+		y=valueY;
 	}
-	 MyString(const MyString& other)
+	bool operator ==(const Point& other)
 	{
-		this->size = other.size;
-		this->arr = new char[this->size + 1];
-		for (int i = 0; i < this->size; i++)
-		{
-			this->arr[i] = other.arr[i];
-		}
-		this->arr[this->size] = '\0';
+		return this->x == other.x && this->y == other.y;
 	}
-	 MyString& operator =(const MyString& other)
-	 {
-		 if (this->arr != nullptr)
-		 {
-			 delete[]arr;
-		 }
-		 this->size = other.size;
-		 this->arr = new char[this->size + 1];
-		 for (int i = 0; i < size; i++)
-		 {
-			 this->arr[i] = other.arr[i];
-		 }
-		 this->arr[this->size] = '\0';
-		 return *this;
-	 }
-	 MyString operator +(const MyString& other)
-	 {
-		 MyString temp;
-		 temp.size = this->size + other.size;
-		 temp.arr = new char[this->size + other.size + 1];
-		 int i = 0;
-		 for(; i < this->size; i++)
-		 {
-			 temp.arr[i] = this->arr[i];
-		 }
-		 for (int j = 0; j < other.size; j++, i++)
-		 {
-			 temp.arr[i] = other.arr[j];
-		 }
-		 temp.arr[temp.size] = '\0';
-		 return temp;
-	 }
-	 MyString& operator +=(const MyString& other)
-	 {
-		 int size1 = this->size;
-		 int size2 = other.size;
-		 MyString temp;
-		 temp.size = size1 + size2;
-		 temp.arr = new char[size1 + size2 + 1];
-		 int i = 0;
-		 for (; i < size1; i++)
-		 {
-			 temp.arr[i] = this->arr[i];
-		 }
-		 for (int j = 0; j < size2; j++, i++)
-		 {
-			 temp.arr[i] = other.arr[j];
-		 }
-		 temp.arr[temp.size] = '\0';
-		 if (this->arr != nullptr)
-		 {	 
-			 delete[] this->arr;
-		 }
-		 this->size = size1+size2;
-		 this->arr = new char[size1+size2+1];
-		 for (int i = 0; i < this->size; i++)
-		 {
-			 this->arr[i] = temp.arr[i];
-		 }
-		 this->arr[this->size] = '\0';
-		 return* this;
-	 }
-	 void Print() {
-		 cout<< arr << endl;
-	 }
-	~MyString()
+	bool operator !=(const Point& other)
 	{
-			delete[]arr;
+		return !(this->x == other.x && this->y == other.y);
+	}
+	bool operator <(const Point& other)
+	{
+		return(this->x < other.x && this->y < other.y);
+	}
+	bool operator >(const Point& other)
+	{
+		return(this->x > other.x && this->y > other.y);
+	}
+	Point operator +(const Point& const  other)
+	{
+		Point temp;
+		temp.x = this->x + other.x;
+		temp.y = this->y + other.y;
+		return temp;
+	}
+	
+	Point& operator ++()   //prefixed increment
+	{
+		this->x++;
+		this->y++;
+		return*this;
+	}
+	Point operator ++(int value)   //postfixed increment
+	{
+		Point temp;
+		temp.x = this->x;
+		temp.y = this->y;
+			this->x++;
+			this->y++;
+		return temp;
+	}
+	Point& operator --()   //prefixed decrement
+	{
+		this->x--;
+		this->y--;
+		return*this;
+	}
+	Point operator --(int value)   //postfixed decrement
+	{
+		Point temp1(*this);
+		this->x--;
+		this->y--;
+		return temp1;
+	}
+	void Print()
+	{
+		cout << x << ":" << y << endl;
 	}
 private:
-	int size;
-	char* arr;
+	int x;
+	int y;
 };
+
 
 int main()
 {
-	MyString s1("October ");
-	MyString s2("sun");
-	
-	s1.Print();
-	s2.Print();
-	cout << "= operator. Answer of s1 = s2 is:" << endl;
-	s1 = s2;
-	s1.Print();
-	s1.Print();
-	cout << endl;
+	Point a(10, 19);
+	Point b(20, 7);
+	Point c(10, 19);
 
-	MyString s3("Black ");
-	MyString s4("see");
-	s3.Print();
-	s4.Print();
-	cout << "+operator. Answer of 'result = s3 + s4' is:" << endl;
-	MyString result;
-	result = s3 + s4;
-	result.Print();
-	cout << endl;
-	
-	MyString s5("My dog ");
-	MyString s6("is barking now.");
-	s5.Print();
-	s6.Print();
-	cout << "+= operator. Answer of s5+=s6 is:" << endl;
-	s5 += s6;
-	s5.Print();
-	
+	bool result1 = (a == b);
+	bool result2 = (a == c);
 
+	cout<< result1;
+	cout << result2;
+	cout << endl;
+	Point d;
+	d = a + b;
+	d.Print();
+	++d;
+	d.Print();
+	bool result3 = (a < d);
+	cout << result3;
+	cout << endl;
+	bool result4 = (b > c);
+	cout << result4;
+
+	a = b;
+	cout << endl;
+	a.Print();
+	d.Print();
+	b =a  + d--;
+	b.Print();
+	d.Print();
 	return 0;
+	
 }
