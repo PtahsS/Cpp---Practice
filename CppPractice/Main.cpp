2fb190ce00f65d44f131fcc97e92d1d9a5b68e3d
PtahS <ptahsrg@gmail.com>
2019-12-03 19:20:29 +0200

    Indexing operator overload

--- CppPractice/Main.cpp
+++ CppPractice/Main.cpp
@@ -1,113 +1,25 @@
 #include <iostream>
 using namespace std;
+// Indexing operator overload
 
-// Overloading operators (increment, decrement...)
-
-class Point
+class TestClass
 {
 public:
-	Point()
-	{
-		x = 0;
-		y = 0;
-	}
-	Point(int valueX, int valueY)
-	{
-		x = valueX;
-		y=valueY;
-	}
-	bool operator ==(const Point& other)
-	{
-		return this->x == other.x && this->y == other.y;
-	}
-	bool operator !=(const Point& other)
-	{
-		return !(this->x == other.x && this->y == other.y);
-	}
-	bool operator <(const Point& other)
-	{
-		return(this->x < other.x && this->y < other.y);
-	}
-	bool operator >(const Point& other)
-	{
-		return(this->x > other.x && this->y > other.y);
-	}
-	Point operator +(const Point& const  other)
-	{
-		Point temp;
-		temp.x = this->x + other.x;
-		temp.y = this->y + other.y;
-		return temp;
-	}
-	
-	Point& operator ++()   //prefixed increment
-	{
-		this->x++;
-		this->y++;
-		return*this;
-	}
-	Point operator ++(int value)   //postfixed increment
-	{
-		Point temp;
-		temp.x = this->x;
-		temp.y = this->y;
-			this->x++;
-			this->y++;
-		return temp;
-	}
-	Point& operator --()   //prefixed decrement
-	{
-		this->x--;
-		this->y--;
-		return*this;
-	}
-	Point operator --(int value)   //postfixed decrement
-	{
-		Point temp1(*this);
-		this->x--;
-		this->y--;
-		return temp1;
-	}
-	void Print()
-	{
-		cout << x << ":" << y << endl;
+	int& operator [](int index) // Если убрать амперсанд, 
+	{							//мы не сможем менять значения массива.
+		return arr[index];
 	}
 private:
-	int x;
-	int y;
+	int arr[5]{5, 44, 4, 987, 69};
 };
-
 
 int main()
 {
-	Point a(10, 19);
-	Point b(20, 7);
-	Point c(10, 19);
+	setlocale(LC_ALL, "ru");
+	TestClass a;
+	cout << a[2] << endl;
+	a[2] = 100;
+	cout << a[2] << endl;
 
-	bool result1 = (a == b);
-	bool result2 = (a == c);
-
-	cout<< result1;
-	cout << result2;
-	cout << endl;
-	Point d;
-	d = a + b;
-	d.Print();
-	++d;
-	d.Print();
-	bool result3 = (a < d);
-	cout << result3;
-	cout << endl;
-	bool result4 = (b > c);
-	cout << result4;
-
-	a = b;
-	cout << endl;
-	a.Print();
-	d.Print();
-	b =a  + d--;
-	b.Print();
-	d.Print();
 	return 0;
-	
 }
